fddc1a89593453708c9cda297bd59fcbba7178d0
Mariam-Lobzhanidze <mariami.lobzhanidze7@gmail.com>
2025-06-20 00:49:09 +0400

    updated env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://adopto-backend.onrender.com/api',
+  // apiUrl: 'https://adopto-backend.onrender.com/api',
+  apiUrl: 'https://pet-adoption-backend-1-ltxg.onrender.com/api',
 };
